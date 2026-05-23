7d76a5d55628aa888b817129b8c790ae2603d983
forritu72 <65449964+forritu72@users.noreply.github.com>
2026-05-23 08:20:14 -0400

    Update S3 bucket and region in backend configuration
    my bucket

--- eks/eks-infra/backend.tf
+++ eks/eks-infra/backend.tf
@@ -1,8 +1,8 @@
 terraform {
     backend "s3" {
-        bucket         = "state-bucket-879381241087"
+        bucket         = "amzn-terraform-demo"
         key            = "k8sbootcamp-march26/eks/eks-infra/terraform.tfstate"
-        region         = "ap-south-1"
+        region         = "us-east-1"
         encrypt        = true
         use_lockfile = true
     }
